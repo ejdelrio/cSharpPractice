a181796917991d0e5773a5db34550dab549eb544
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-27 15:21:22 -0800

    Debugged static method on BST class BuildTreeFromSortedArray

--- cSharpPractice/BSTNode.cs
+++ cSharpPractice/BSTNode.cs
@@ -32,41 +32,36 @@ namespace cSharpPractice
 
     public void printTree() {
       List<BSTNode> nodeList = new List<BSTNode>();
+      Console.WriteLine("-------------");
 
       nodeList.Add(this);
       //Performs a level order traversal and prints each value by level.
       while(nodeList.Count > 0) {
         BSTNode currentNode = nodeList[0];
         nodeList.RemoveAt(0);
-
         if (currentNode.left != null) nodeList.Add(currentNode.left);
         if (currentNode.right != null) nodeList.Add(currentNode.right);
-
         Console.WriteLine(currentNode.val); 
       }
     }
 
     public static BSTNode BuildFromSortedArr(int[] arr) {
       if (arr.Length == 0) throw new ArgumentException("Array must not be empty");
-      int middle = arr.Length / 2;
-      if (middle == 0) return new BSTNode(middle);
-
-      BSTNode root = new BSTNode(arr[middle]);
 
       BSTNode BuildTreeRecursion(int start, int end) {
-        
-        int mid = ((end - start) / 2 + start);
+        int range = end - start;
+        int rounder = range % 2 != 0 ? 1 : 0;
+        int mid = (range / 2 + start) + rounder;
+    
         BSTNode childNode = new BSTNode(arr[mid]);
-
         if (start <= mid - 1) childNode.left = BuildTreeRecursion(start, mid - 1);
         if (mid + 1 <= end) childNode.right = BuildTreeRecursion(mid + 1, end);
-
         return childNode;
+
       }
 
-      root.left = BuildTreeRecursion(0, middle - 1);
-      root.right = BuildTreeRecursion(middle + 1, arr.Length - 1);
-      return root;
+
+      return BuildTreeRecursion(0, arr.Length - 1);
     }
   }
 }
