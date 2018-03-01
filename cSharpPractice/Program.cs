a814c6440f003d0814b533edff4093634a1cd4a5
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-28 16:24:35 -0800

    Commented out all class instances and method calls. Console.Wrote Hello World :D

--- cSharpPractice/Program.cs
+++ cSharpPractice/Program.cs
@@ -6,29 +6,32 @@ namespace cSharpPractice
   {
     public static void Main()
     {
-      Matrix m = new Matrix(8);
-      m.printMatrix();
-      m.rotateMatrix();
-      m.printMatrix();
 
-      BSTNode root = new BSTNode(6);
-      root.printTree();
-      int[] sortedArr = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-      int[] unsortedArr = { 3, 2, 4, 1, 6 };
-      BSTNode treeFromArr = BSTNode.BuildFromSortedArr(sortedArr);
-      //BSTNode willThrowError = BSTNode.BuildFromSortedArr(unsortedArr);
-      treeFromArr.printTree();
+      Console.WriteLine("Hello World!");
+      Console.ReadKey();
+      //Matrix m = new Matrix(8);
+      //m.printMatrix();
+      //m.rotateMatrix();
+      //m.printMatrix();
 
-      PersonTrieTree contactBook = new PersonTrieTree();
+      //BSTNode root = new BSTNode(6);
+      //root.printTree();
+      //int[] sortedArr = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+      //int[] unsortedArr = { 3, 2, 4, 1, 6 };
+      //BSTNode treeFromArr = BSTNode.BuildFromSortedArr(sortedArr);
+      ////BSTNode willThrowError = BSTNode.BuildFromSortedArr(unsortedArr);
+      //treeFromArr.printTree();
 
-      contactBook.AddContact("Eddie", 7604683448);
-      contactBook.AddContact("Eric", 2342342345);
-      contactBook.AddContact("Edgar", 6666666666);
-      contactBook.AddContact("Sylvia", 6306742391);
-      contactBook.AddContact("Emily", 4444444444);
-      contactBook.AddContact("Edwin", 4168906994);
+      //PersonTrieTree contactBook = new PersonTrieTree();
 
-      contactBook.displayFuzzySearchresults("Ed");
+      //contactBook.AddContact("Eddie", 7604683448);
+      //contactBook.AddContact("Eric", 2342342345);
+      //contactBook.AddContact("Edgar", 6666666666);
+      //contactBook.AddContact("Sylvia", 6306742391);
+      //contactBook.AddContact("Emily", 4444444444);
+      //contactBook.AddContact("Edwin", 4168906994);
+
+      //contactBook.displayFuzzySearchresults("Ed");
 
     }
   }
